8e49c7ea707023dc21edc79c1afe11c88a305eb6
oka687 <oka684@naver.com>
2021-11-06 19:23:21 +0900

    ㄴㄴ

---
+++ src/first.java
@@ -0,0 +1,39 @@
+public class first {
+
+    public void firstMoon() {
+
+        long start = System.nanoTime();
+
+        int money = 5680;
+        int[] change = new int[4];
+        change[0] = 500;
+        change[1] = 100;
+        change[2] = 50;
+        change[3] = 10;
+
+        int calcCoin = 0; // coin count(몫)
+
+        for (int i = 0; i < change.length; i++) {
+
+            int mock = money / change[i];
+            int naMerge = money % change[i];
+
+            calcCoin = calcCoin + mock; // 코인개수 카운트
+
+            if (naMerge != 0) {
+
+                money = naMerge; // 나머지를 계산식에 대입
+                continue;
+            } else {
+                System.out.println("동전개수 : " + calcCoin);
+                break;
+            }
+
+        }
+
+        long end = System.nanoTime();
+
+        System.out.println(end - start);
+    }
+
+}
