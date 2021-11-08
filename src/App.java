b6cbab680184f50928cb0f56de5b9e1d2af302d5
oka687 <oka684@naver.com>
2021-11-08 21:20:38 +0900

    상하좌우 두번째

--- src/App.java
+++ src/App.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class App {
@@ -8,11 +7,6 @@ public class App {
         int map = 0;
         int x = 1;
         int y = 1;
-        int count = 0;
-
-        boolean whileSwitch = true;
-
-        List<String> arrow = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
 
@@ -26,64 +20,22 @@ public class App {
         for (int i = 0; i < arry.length; i++) {
             if (arry[i] == "L") {
 
-                if (y - 1 <= 0) {
-                    continue;
-                }
-                y--;
-                count++;
+                y = y - 1 <= 0 ? y : y--;
+
             } else if (arry[i] == "R") {
-                if (y + 1 > map) {
-                    continue;
-                }
-                y++;
-                count++;
+
+                y = y + 1 > map ? y : y++;
+
             } else if (arry[i] == "U") {
-                if (x - 1 <= 0) {
-                    continue;
-                }
-                x--;
-                count++;
+
+                x = x - 1 <= 0 ? x : x--;
+
             } else if (arry[i] == "D") {
-                if (x + 1 > map) {
-                    continue;
-                }
-                x++;
-                count++;
+
+                x = x + 1 > map ? x : x++;
             }
-
         }
-
-        System.out.println("N = " + count);
         System.out.println("x :" + x + " " + "y : " + y);
-
-        // Scanner arrowList = new Scanner(System.in);
-        // System.out.println("방향키 입력 N을 입력 시, 종료");
-
-        // while (whileSwitch) {
-
-        // switch (arrowList.nextLine()) {
-        // case "L":
-        // arrow.add(arrowList.nextLine());
-        // break;
-        // case "R":
-        // arrow.add(arrowList.nextLine());
-        // break;
-        // case "U":
-        // arrow.add(arrowList.nextLine());
-        // break;
-        // case "D":
-        // arrow.add(arrowList.nextLine());
-        // break;
-        // case "N":
-        // whileSwitch = false;
-        // default:
-        // System.out.println("다시 입력");
-        // }
-        // }
-
-        // arrowList.close();
-
-        // System.out.println(arrow);
 
     }
 
