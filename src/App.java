7180cc94153a25b91226e3a72eb6a651c00a32c2
oka687 <oka684@naver.com>
2021-11-10 00:59:38 +0900

    시각

--- src/App.java
+++ src/App.java
@@ -1,34 +1,24 @@
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int map = 0;
-        int x = 1;
-        int y = 1;
+        // 입력되는 시간(hour)
+        int N = 5;
+        int count = 0;
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("N의 크기 입력");
-        map = sc.nextInt();
-
-        sc.close();
-
-        String[] arry = { "D", "D", "D", "L", "U", "U", "U", "R" };
-
-        for (String num : arry) {
-            if (num == "L") {
-                y += y - 1 <= 0 ? y : y--;
-            } else if (num == "R") {
-                y = y + 1 > map ? y : y++;
-            } else if (num == "U") {
-                x = x - 1 <= 0 ? x : x--;
-            } else if (num == "D") {
-                x = x + 1 > map ? x : x++;
+        for (int h = 0; h <= N; h++) {
+            for (int m = 0; m < 60; m++) {
+                for (int s = 0; s < 60; s++) {
+                    String hh = String.format("%02d", h);
+                    String mm = String.format("%02d", m);
+                    String ss = String.format("%02d", s);
+                    String hhmmss = hh + mm + ss;
+                    if (hhmmss.indexOf("3") > 0) {
+                        count++;
+                    }
+                }
             }
         }
-        System.out.println("x :" + x + " " + "y : " + y);
+        System.out.println(count);
     }
-
 }
