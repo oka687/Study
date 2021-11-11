3ec0dcec1c6a8f556f99684539853063b1dfc0f1
oka687 <oka684@naver.com>
2021-11-11 21:12:16 +0900

    맵만들기

--- src/App.java
+++ src/App.java
@@ -1,24 +1,56 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // 입력되는 시간(hour)
-        int N = 5;
-        int count = 0;
+        // 맵 리스트
+        int N = 0;
+        int M = 0;
 
-        for (int h = 0; h <= N; h++) {
-            for (int m = 0; m < 60; m++) {
-                for (int s = 0; s < 60; s++) {
-                    String hh = String.format("%02d", h);
-                    String mm = String.format("%02d", m);
-                    String ss = String.format("%02d", s);
-                    String hhmmss = hh + mm + ss;
-                    if (hhmmss.indexOf("3") > 0) {
-                        count++;
-                    }
-                }
+        // 캐릭터 방향
+        int x = 1;
+        int y = 1;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("N의 개수를 입력");
+            N = sc.nextInt();
+
+            System.out.println("M의 개수를 입력");
+            M = sc.nextInt();
+
+            if (3 > N || M > 50) {
+                System.out.println("다시 입력해주세요.");
+            } else {
+                break;
             }
         }
-        System.out.println(count);
+
+        int[][] map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < M; k++) {
+                // 랜덤으로 육지 또는 바다 결정
+                int ran = (int) ((Math.random() * 10000) % 10);
+
+                // 테두리에 해당하는 부분은 전부 바다로 처리
+                if (i == 0 || i == N - 1 || k == 0 || k == M - 1) {
+                    map[i][k] = 0;
+                    System.out.print(map[i][k]);
+                } else if (ran <= 5) {
+                    // 50% 확률로 육지
+                    map[i][k] = 1;
+                    System.out.print(map[i][k]);
+                } else {
+                    // 50% 확률로 바다
+                    map[i][k] = 0;
+                    System.out.print(map[i][k]);
+                }
+            }
+            System.out.println("");
+        }
+
     }
+
 }
